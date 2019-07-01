8b3fcd5ade55468b61f874059f0e518698e01063
VlTre <vladdd3@gmail.com>
2019-07-01 20:15:14 +0300

    initial commit

---
+++ precache-manifest.8d27982e2a8433c02b56ca3dc3b9843f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12702582197c0d37730145288c8f5ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2591eeaa555103647850",
+    "url": "/static/css/main.fda59e02.chunk.css"
+  },
+  {
+    "revision": "a55cb95024b9f5b1672c",
+    "url": "/static/js/2.361b8403.chunk.js"
+  },
+  {
+    "revision": "2591eeaa555103647850",
+    "url": "/static/js/main.f3a67ca9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5630cf13bdd3989e49520258fec33a8",
+    "url": "/static/media/Bitcoin-icon.a5630cf1.png"
+  }
+]);
