2021747a81a88bf6e9045929d2696e1c7105ec9a
VlTre <vladdd3@gmail.com>
2019-07-04 18:17:12 +0300

    update fix

---
+++ precache-manifest.8442f4c655ac32fe015855323a1118d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0400d7edf0ff74519a3ea5419c3da23e",
+    "url": "/bitcoin-info-demo/index.html"
+  },
+  {
+    "revision": "11bee73bb6353f956550",
+    "url": "/bitcoin-info-demo/static/css/main.fda59e02.chunk.css"
+  },
+  {
+    "revision": "a55cb95024b9f5b1672c",
+    "url": "/bitcoin-info-demo/static/js/2.361b8403.chunk.js"
+  },
+  {
+    "revision": "11bee73bb6353f956550",
+    "url": "/bitcoin-info-demo/static/js/main.efd75945.chunk.js"
+  },
+  {
+    "revision": "46a1cb94b1ad229d5f7b",
+    "url": "/bitcoin-info-demo/static/js/runtime~main.a49e34a3.js"
+  },
+  {
+    "revision": "a5630cf13bdd3989e49520258fec33a8",
+    "url": "/bitcoin-info-demo/static/media/Bitcoin-icon.a5630cf1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3f0af068787ea98167ca261ce334dc05.js"
+  "/bitcoin-info-demo/precache-manifest.8442f4c655ac32fe015855323a1118d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bitcoin-info-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
